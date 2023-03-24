f8bae736b183cc92d1d1a10014bbd7d54f34bb11
Ruben Martins <rubenmachadomartins48@gmail.com>
2023-03-24 10:55:03 +0000

    Refactorzito & Adição de Valores Ficheiro strings.xml
    Refactorzito no Nomes dos Fragmentos (Home , Biography & Mapa)
    Adição de Valores ao ficheiro strings.xml

--- app/src/main/java/com/example/raullino/ui/biography/BiographyViewModel.kt
+++ app/src/main/java/com/example/raullino/ui/biography/BiographyViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.raullino.ui.notifications
+package com.example.raullino.ui.biography
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class BiographyViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Biography Fragment"
     }
     val text: LiveData<String> = _text
 }
