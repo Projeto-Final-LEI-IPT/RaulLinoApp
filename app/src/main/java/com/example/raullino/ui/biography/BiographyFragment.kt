c1ead4f28eab59d27d302d42878e02330fb5bbdd
Ruben Martins <rubenmachadomartins48@gmail.com>
2023-03-28 17:57:47 +0100

    Ajuste Texto Biography

--- app/src/main/java/com/example/raullino/ui/biography/BiographyFragment.kt
+++ app/src/main/java/com/example/raullino/ui/biography/BiographyFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.raullino.databinding.FragmentDashboardBinding
+import com.example.raullino.databinding.FragmentBiographyBinding
 
 class BiographyFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentBiographyBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class BiographyFragment : Fragment() {
         val dashboardViewModel =
                 ViewModelProvider(this).get(BiographyViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentBiographyBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textBiography
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
