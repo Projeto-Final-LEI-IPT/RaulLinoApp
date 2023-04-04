71f49e8042c4b050d6e34999d7ba5a89c04faac2
GoncaloAlpalhao <aluno23048@ipt.pt>
2023-04-04 22:02:37 +0100

    Json Aberto
    Finalmente abri o ficheiro json e consegui faze-lo imprimir a quantidade de objetos que possui.
    
    Nota: Alterei tambem um valor incorreto presente no ficheiro json originalmente fornecido

--- app/src/main/java/com/example/raullino/jsonParse.kt
+++ app/src/main/java/com/example/raullino/jsonParse.kt
@@ -1,4 +1,23 @@
 package com.example.raullino
 
-class jsonParse {
+import android.content.Context
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+
+class jsonParse(val context: Context) {
+
+    //Function to get the number of objects in the json file
+    fun get_number(): Int {
+        var json: String? = null
+        try {
+            val inputStream: InputStream = context.assets.open("data.json")
+            json = inputStream.bufferedReader().use { it.readText() }
+        } catch (e: IOException) {
+        }
+        val json_object = JSONObject(json)
+        val json_array = json_object.getJSONArray("dados")
+        val number = json_array.length()
+        return number
+    }
 }
