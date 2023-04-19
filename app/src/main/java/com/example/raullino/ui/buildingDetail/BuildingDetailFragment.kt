065532e0e55c3ea95ce4ac06c978484e809d03b1
rDg06 <rodrigogracacosta@hotmail.com>
2023-04-19 18:44:21 +0100

    Indentação de código

--- app/src/main/java/com/example/raullino/ui/buildingDetail/BuildingDetailFragment.kt
+++ app/src/main/java/com/example/raullino/ui/buildingDetail/BuildingDetailFragment.kt
@@ -16,14 +16,13 @@ class BuildingDetailFragment : Fragment() {
     lateinit var textTitle: TextView
     lateinit var textInfo: TextView
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentBuildingDetailBinding.inflate(inflater, container, false)
