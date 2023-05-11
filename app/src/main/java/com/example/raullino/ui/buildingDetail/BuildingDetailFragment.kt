70ceda6efff25561b818332f09f0f28fcdc7eb6d
FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>
2023-05-11 10:46:56 +0100

    Implementação do image_slider

--- app/src/main/java/com/example/raullino/ui/buildingDetail/BuildingDetailFragment.kt
+++ app/src/main/java/com/example/raullino/ui/buildingDetail/BuildingDetailFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.raullino.JsonParse
 import com.example.raullino.databinding.FragmentBuildingDetailBinding
 
@@ -15,6 +17,14 @@ class BuildingDetailFragment : Fragment() {
 
     lateinit var textTitle: TextView
     lateinit var textInfo: TextView
+    private lateinit var imageViewPagerAdapter: ImageViewPagerAdapter
+    val imageUrlList = listOf(
+        "https://images.unsplash.com/photo-1621318164984-b06589834c91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+        "https://images.unsplash.com/photo-1621551122354-e96737d64b70?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+        "https://images.unsplash.com/photo-1621616875450-79f024a8c42c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+        "https://images.unsplash.com/photo-1621687947404-e41b3d139088?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxOTU3MDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjM2OTk4MjI&ixlib=rb-1.2.1&q=80&w=1080",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTSmH3IM9oI8b7p0TOj0rXQs1l7S4XuUKWBw&usqp=CAU"
+    )
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
@@ -33,14 +43,50 @@ class BuildingDetailFragment : Fragment() {
         textTitle = binding.textTitle
         textInfo = binding.textInfo
 
-        textTitle.text = jsonParse.get_title("3");
-        textInfo.text = jsonParse.get_info("3");
+        textTitle.text = jsonParse.get_title("3")
+        textInfo.text = jsonParse.get_info("3")
+
+        //initializing the adapter
+        imageViewPagerAdapter = ImageViewPagerAdapter(imageUrlList)
+
+        setUpViewPager() // Adicionar esta linha
 
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setUpViewPager() {
+
+        binding.viewPager.adapter = imageViewPagerAdapter
+
+        //set the orientation of the viewpager using ViewPager2.orientation
+        binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+
+        //select any page you want as your starting page
+        val currentPageIndex = 0
+        binding.viewPager.currentItem = currentPageIndex
+
+        // registering for page change callback
+        binding.viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                    //update the image number textview
+                    binding.imageNumberTV.text = "${position + 1} / ${imageUrlList.size}"
+                }
+            }
+        )
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // unregistering the onPageChangedCallback
+        binding.viewPager.unregisterOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {}
+        )
+    }
+
 }
---
+++ app/src/main/java/com/example/raullino/ui/buildingDetail/ViewPagerAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.raullino.ui.buildingDetail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.raullino.databinding.ImageItemBinding
+
+class ImageViewPagerAdapter(private val imageUrlList: List<String>) :
+    RecyclerView.Adapter<ImageViewPagerAdapter.ViewPagerViewHolder>() {
+
+    inner class ViewPagerViewHolder(val binding: ImageItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun setData(imageUrl: String) {
+
+            Glide.with(binding.root.context)
+                .load(imageUrl)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(binding.ivImage)
+        }
+
+    }
+
+    override fun getItemCount(): Int = imageUrlList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+
+        val binding = ImageItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ViewPagerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+
+        holder.setData(imageUrlList[position])
+    }
+
+}
