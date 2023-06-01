1d68ee53092c41f8b8c4cc53f164dde00a32c051
FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>
2023-06-01 21:41:45 +0100

    Tradução de conteudos estaticos

--- app/src/main/java/com/example/raullino/ui/biography/BiographyFragment.kt
+++ app/src/main/java/com/example/raullino/ui/biography/BiographyFragment.kt
@@ -1,6 +1,7 @@
 package com.example.raullino.ui.biography
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,7 @@ import com.example.raullino.databinding.FragmentBiographyBinding
 class BiographyFragment : Fragment() {
 
     private var _binding: FragmentBiographyBinding? = null
-
+    val flag: Boolean = true
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -24,6 +25,16 @@ class BiographyFragment : Fragment() {
         _binding = FragmentBiographyBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val TextView = binding.biografia
+        val TextView2 = binding.titulo
+        val TextView3 = binding.dados
+
+        if(flag){
+            TextView.text="He was a unique personality on the Portuguese art scene, largely due to the fact that he managed to articulate the Portuguese tradition with the innovative European currents of the early 20th century. [2] With 70 years of professional activity, Lino is the author of more than 700 works. It is also important to mention that despite his range of projects, he was also a man with a vast theoretical or written work, which became very determinant, for his followers over decades in Portugal."
+            val htmlText = "<b>Full name:</b><br/>Raul Lino da Silva<br/><b>Birth:</b> November 21, 1879, Lisbon<br/><b>Death:</b><br/>July 13, 1974 (94 years old), Lisbon"
+            TextView3.text = Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY)
+            TextView2.text="Biographical note";
+        }
         return root
     }
 
--- app/src/main/java/com/example/raullino/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/raullino/ui/home/HomeFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.raullino.R
 import com.example.raullino.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
+    val flag: Boolean = true
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -25,6 +27,20 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val TextView4 = binding.textView4
+        val TextView3 = binding.textView3
+        val TextView2 = binding.textView2
+        val TextView = binding.textView
+        val fw =he
+
+
+        if(flag){
+            TextView4.text = "The begining"
+            TextView.text="A master of Portuguese architecture, he imprints life in iconic arches. Graceful and solid arches transcend time, revealing a singular beauty."
+            TextView3.text="It stands out for its imposing vertical lines. Magnificent columns that convey grandeur and sophistication in their timeless designs";
+            TextView2.text="Its windows are frames for discovery. They reveal breathtaking landscapes, inviting you to explore the magic beyond their openings.";
+        }
 
         return root
     }
