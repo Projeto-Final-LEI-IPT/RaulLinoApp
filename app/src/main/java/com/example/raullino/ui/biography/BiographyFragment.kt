15a42f15996a37191c58cb76fa57349162bd8d17
rDg06 <rodrigogracacosta@hotmail.com>
2023-06-05 19:13:21 +0100

    Correções

--- app/src/main/java/com/example/raullino/ui/biography/BiographyFragment.kt
+++ app/src/main/java/com/example/raullino/ui/biography/BiographyFragment.kt
@@ -31,10 +31,10 @@ class BiographyFragment : Fragment() {
         val TextView3 = binding.dados
 
         if(Flags.selectedFlag=="EN"){
-            TextView.text="He was a unique personality on the Portuguese art scene, largely due to the fact that he managed to articulate the Portuguese tradition with the innovative European currents of the early 20th century. [2] With 70 years of professional activity, Lino is the author of more than 700 works. It is also important to mention that despite his range of projects, he was also a man with a vast theoretical or written work, which became very determinant, for his followers over decades in Portugal."
+            TextView.text="He was a unique personality on the Portuguese art scene, largely due to the fact that he managed to articulate the Portuguese tradition with the innovative European currents of the early 20th century. With 70 years of professional activity, Lino is the author of more than 700 works. It is also important to mention that despite his range of projects, he was also a man with a vast theoretical or written work, which became very determinant, for his followers over decades in Portugal."
             val htmlText = "<b>Full name:</b><br/>Raul Lino da Silva<br/><b>Birth:</b> November 21, 1879, Lisbon<br/><b>Death:</b><br/>July 13, 1974 (94 years old), Lisbon"
             TextView3.text = Html.fromHtml(htmlText, Html.FROM_HTML_MODE_LEGACY)
-            TextView2.text="Biographical note";
+            TextView2.text="Biographical Note";
         }
         return root
     }
